adc53e51cc47d1d5d62b2d0d5360b9aa8c1165dd
Chatterji <arinchat@3c22fbbe8bd6.ant.amazon.com>
2021-03-02 16:54:22 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World from Arindam!'));
+app.get('/', (req, res) => res.send('Hello World from Arindam and Alicia!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
